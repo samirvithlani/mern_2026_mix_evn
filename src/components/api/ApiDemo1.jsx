97b29217f6cadb2e8521b63c8bac88e4a78cebdf
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-01 17:19:38 +0530

    api demo1

--- src/components/api/ApiDemo1.jsx
+++ src/components/api/ApiDemo1.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ApiDemo1 = () => {
+
+  const [message, setmessage] = useState("")
+  const [users, setusers] = useState([])
 
   //create udf function
   const getUsers =async()=>{
@@ -12,8 +15,10 @@ export const ApiDemo1 = () => {
     console.log("res..axios object..",res)
     //api response res.data
     console.log("api response...",res.data)
-    console.log("message variable..",res.data.message)
-    console.log("data..",res.data.data)
+    console.log("message variable..",res.data.message)//""
+    console.log("data..",res.data.data) //[]
+    setmessage(res.data.message)
+    setusers(res.data.data)
 
   }
 
@@ -21,7 +26,14 @@ export const ApiDemo1 = () => {
     <div style={{textAlign:"center"}}>
         <h1>API DEMO 1</h1>
         <h3>GET API</h3>
+        <h1>{message}</h1>
         <button onClick={getUsers}>GET</button>
+        {
+          //user -{}
+          users.map((user)=>{
+            return <li>{user.name} ----- {user.age}</li>
+          })
+        }
     </div>
   )
 }
